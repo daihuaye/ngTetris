29f389da1d78a026645128d50fdec509db5cc4f9
daihua ye <davishacker86@gmail.com>
2014-10-07 22:08:26 -0700

    add different mode in game.

---
+++ src/common/services/speed.js
@@ -0,0 +1,13 @@
+/**
+* service.GAMESPEED Module
+*
+* Description
+*/
+angular.module('service.GAMESPEED', [])
+.constant('GAMESPEED', {
+    'BEGINNER': 800,
+    'INTERMEDIATE': 500,
+    'ADVANCED': 200,
+    'EXPERT': 180,
+    'SUPER': 150
+});
