ddf849246aa84f207240bbeb0d097527e0448fd8
daihua ye <davishacker86@gmail.com>
2014-10-07 22:08:51 -0700

    Add diBootstrapSlider directive

---
+++ src/common/directives/diBootstrapSlider/diBootstrapSlider.js
@@ -0,0 +1,48 @@
+/**
+* directive.diBootstrapSlider Module
+*
+* Description
+*/
+angular.module('directive.diBootstrapSlider', [
+    'service.GameManager',
+    'service.GAMESPEED'
+])
+.controller('BootstrapSliderCtrl', [
+    '$scope',
+    'GameManager',
+    'GAMESPEED',
+function (
+    $scope,
+    GameManager,
+    GAMESPEED
+){
+    this.updateGameSpeed = function updateGameSpeed(option) {
+        var speed = GAMESPEED[option];
+        GameManager.updateGameSpeed(speed);
+    };
+}])
+.directive('diBootstrapSlider', [
+function(
+){
+    var BootstrapSlider = {};
+
+    BootstrapSlider.controller = 'BootstrapSliderCtrl';
+    
+    BootstrapSlider.templateUrl = 'directives/diBootstrapSlider/diBootstrapSlider.tpl.html';
+
+    BootstrapSlider.restrict = 'A';
+
+    BootstrapSlider.scope = true;
+
+    BootstrapSlider.replace = true;
+
+    BootstrapSlider.link = function link(scope, element, attrs, controller) {
+        element.on('click', function (event) {
+            var option = $(event.target).find('input').data('role');
+            controller.updateGameSpeed(option);
+            scope.$emit('BootstrapSlider.Speed');
+        });
+    };
+
+    return BootstrapSlider;
+}]);
