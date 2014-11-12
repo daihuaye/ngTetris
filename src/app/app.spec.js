04b717c2cde66ccaaaa41bbcea0aca1c78d36653
daihua ye <daihua.ye@gmail.com>
2014-11-11 19:27:01 -0800

    fix issue in js

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -2,7 +2,7 @@ describe( 'AppCtrl', function() {
   describe( 'isCurrentUrl', function() {
     var AppCtrl, $location, $scope;
 
-    beforeEach( module( 'ngBoilerplate' ) );
+    beforeEach( module( 'ngTetris' ) );
 
     beforeEach( inject( function( $controller, _$location_, $rootScope ) {
       $location = _$location_;
