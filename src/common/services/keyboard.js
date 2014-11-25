708d0f08f4aff0a04850f1d4dffeb90289de7a67
daihua ye <daihua.ye@gmail.com>
2014-11-24 20:58:48 -0800

    keyboard best practice

--- src/common/services/keyboard.js
+++ src/common/services/keyboard.js
@@ -7,46 +7,54 @@ angular.module('service.Keyboard', [])
 .factory('KeyboardService', [
 function (
 ){
-    var keyboard = {},
-        UP = 'up',
-        RIGHT = 'right',
-        DOWN = 'down',
-        LEFT = 'left',
-        SPACE = 'space',
-        ESC = 'esc',
-        P = 'p',
-        keyboardMap = {
-            37: LEFT,
-            38: UP,
-            39: RIGHT,
-            40: DOWN,
-            32: SPACE,
-            27: ESC,
-            80: P
-        };
+    var keyboard = {
+        init: init,
+        on: on,
+        getKey: getKey,
+        keydownAction: keydownAction,
+        _handleKeyEvent: _handleKeyEvent
+    },
+    UP = 'up',
+    RIGHT = 'right',
+    DOWN = 'down',
+    LEFT = 'left',
+    SPACE = 'space',
+    ESC = 'esc',
+    P = 'p',
+    keyboardMap = {
+        37: LEFT,
+        38: UP,
+        39: RIGHT,
+        40: DOWN,
+        32: SPACE,
+        27: ESC,
+        80: P
+    };
 
-    keyboard.init = function init() {
+    return keyboard;
+
+    function init() {
         keyboard.keyEventHandlers = [];
-    };
+    }
 
-    keyboard.getKey = function getKey(which) {
+    function getKey(which) {
         return keyboardMap[which];
-    };
+    }
 
-    keyboard.keydownAction = function keydownAction(event) {
+    function keydownAction(event) {
         var key = keyboardMap[event.which];
 
         if (key) {
             event.preventDefault();
             keyboard._handleKeyEvent(key, event);
         }
-    };
+    }
 
-    keyboard.on = function on(cb) {
+    function on(cb) {
         keyboard.keyEventHandlers.push(cb);
-    };
+    }
 
-    keyboard._handleKeyEvent = function _handleKeyEvent(key, event) {
+    function _handleKeyEvent(key, event) {
         var callbacks = keyboard.keyEventHandlers;
         if (!callbacks) {
             return;
@@ -60,6 +68,5 @@ function (
                 cb(key, event);
             }
         }
-    };
-    return keyboard;
+    }
 }]);
