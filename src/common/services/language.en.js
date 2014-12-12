5ab429ad359287d06f5777b004e3f6c73cdbf7c2
daihua ye <daihua.ye@gmail.com>
2014-12-11 16:06:59 -0800

    add server language

---
+++ src/common/services/language.en.js
@@ -0,0 +1,26 @@
+angular
+.module('service.language', [])
+.constant('en', {
+    // header
+    sub_title: 'Let\'s play tetris.',
+
+    // instruction
+    press: 'PRESS',
+    or: 'OR',
+    pause_keys: 'TO PAUSE AND VIEW/EDIT CONTROLS',
+    
+    // game contact
+    created_by: 'Created by',
+    author: 'Daihua ye',
+    game_description: 'This is the completed redesign version of my previous',
+    inspired_by: 'And the idea for ngTetris is inspired by',
+    on_top_of: 'This version is built on top of ',
+    AngularJS: 'AngularJS',
+
+    // game Menu
+    new_record: 'New Record:',
+    new_game: 'New Game',
+    your_score: 'Your Score:',
+    game_over: 'Game Over',
+    try_again: 'Try again'
+});
