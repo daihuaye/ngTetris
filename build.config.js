63ad7f8267f28893a3ea15a9c0a0dc95fb3ea1dc
daihuaye <daihua.ye@gmail.com>
2015-02-13 10:50:56 -0800

    add bootstrap-modal

--- build.config.js
+++ build.config.js
@@ -73,8 +73,10 @@ module.exports = {
         'vendor/randomColor/randomColor.js',
         'vendor/basil.js/build/basil.min.js',
         'vendor/angular-touch/angular-touch.min.js',
-        'vendor/angular-translate/angular-translate.min.js'
-    ],
+        'vendor/angular-translate/angular-translate.min.js',
+        'vendor/bootstrap-modal/js/bootstrap-modal.js',
+        'vendor/bootstrap-modal/js/bootstrap-modalmanager.js'
+    ], 
     css: [
     ],
     assets: [
